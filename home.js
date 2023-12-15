1885ee7b419307db51d98b243d354eaef3f4df16
VeronicaOS <verols51029@stud.noroff.no>
2023-12-15 01:54:43 +0100

    first

---
+++ home.js
@@ -0,0 +1,17 @@
+// HAMBURGER MENU
+
+const menuOpen = document.querySelector(".menu-open");
+const menuClose = document.querySelector(".menu-close");
+const navigation = document.querySelector(".navigation");
+
+menuOpen.addEventListener("click", function () {
+    navigation.style.display = "flex";
+    menuOpen.classList.add("menu-hide");
+    menuClose.classList.remove("menu-hide");
+});
+
+menuClose.addEventListener("click", function () {
+    navigation.style.display = "none";
+    menuClose.classList.add("menu-hide");
+    menuOpen.classList.remove("menu-hide");
+});
