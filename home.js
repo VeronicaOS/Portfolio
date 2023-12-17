3307d779d01e4c28ba4f93b20df5273e1a25c769
VeronicaOS <verols51029@stud.noroff.no>
2023-12-17 17:34:31 +0100

    fourth

--- home.js
+++ home.js
@@ -3,6 +3,7 @@
 const menuOpen = document.querySelector(".menu-open");
 const menuClose = document.querySelector(".menu-close");
 const navigation = document.querySelector(".navigation");
+const links = document.querySelectorAll(".navigation a");
 
 menuOpen.addEventListener("click", function () {
     navigation.style.display = "flex";
@@ -14,4 +15,12 @@ menuClose.addEventListener("click", function () {
     navigation.style.display = "none";
     menuClose.classList.add("menu-hide");
     menuOpen.classList.remove("menu-hide");
+});
+
+links.forEach(function (link) {
+    link.addEventListener("click", function () {
+        navigation.style.display = "none";
+        menuClose.classList.add("menu-hide");
+        menuOpen.classList.remove("menu-hide");
+    });
 });
