fd0e0d630670a2e35af19169efa72e303fd4e36e
VeronicaOS <verols51029@stud.noroff.no>
2023-12-17 19:25:37 +0100

    last

--- home.js
+++ home.js
@@ -6,20 +6,20 @@ const navigation = document.querySelector(".navigation");
 const links = document.querySelectorAll(".navigation a");
 
 menuOpen.addEventListener("click", function () {
-    navigation.style.display = "flex";
+    navigation.classList.add("nav-open");
     menuOpen.classList.add("menu-hide");
     menuClose.classList.remove("menu-hide");
 });
 
 menuClose.addEventListener("click", function () {
-    navigation.style.display = "none";
+    navigation.classList.remove("nav-open");
     menuClose.classList.add("menu-hide");
     menuOpen.classList.remove("menu-hide");
 });
 
 links.forEach(function (link) {
     link.addEventListener("click", function () {
-        navigation.style.display = "none";
+        navigation.classList.remove("nav-open");
         menuClose.classList.add("menu-hide");
         menuOpen.classList.remove("menu-hide");
     });
